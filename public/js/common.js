03e79c61b5851bff0a650911923c53480ea6585d
sansuDeSu <syc_kblk@126.com>
2017-09-19 11:20:43 +0800

    初始化1.0.1版本

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,40 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	//实现退出功能
+	//当点击退出按键时
+	$("#loginOut").click(function(){
+		//发送请求
+		$.ajax({
+			type:"post",
+			url:"/api/logout",
+			success:function(data){
+				if(data.code == 200){
+					//请求成功后会删除id
+					console.log("请求成功后会删除id");
+					//跳转登录页
+					location.href = 'main/index';
+				}
+			}
+
+		})
+
+	})
+
+	//全局下检测用户是否有登录,若没有sessid信息,只能在登录页
+	var ret = $.cookie("PHPSESSID");
+	if(!ret){
+		console.log("没有id")
+		location.href = '/login';
+	};
+	//设置用户信息
+		//拿到cookie里存放的数据,
+	console.log($.cookie("loginInfo"));
+	var loginInfo = $.cookie("loginInfo");
+		//转成对象格式,
+	loginInfo = loginInfo && JSON.parse(loginInfo);
+		//添加到页面中
+	$(".aside .profile img").attr("src",loginInfo.tc_avatar);
+	$(".aside .profile h4").html(loginInfo.tc_name);
+
