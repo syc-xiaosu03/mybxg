37a9d5f0b0f2e490c3eb6d250ce26cbdeea8702f
sansuDeSu <syc_kblk@126.com>
2017-09-21 21:11:44 +0800

    完成注销与启用功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,9 +8,48 @@ define(["jquery","template"],function($,template){
          url:"/api/teacher",
         dataType : "json",
         success : function(data){
-            console.log(data);
+
             var html = template("tachertpl",{list:data.result});
             $("#teacherInfo").html(html);
+            //渲染成功后,绑定点击事件,实现注销功能
+            $(".eod").click(function(){
+                var that = $(this);
+                //获取其父元素
+                var td = that.closest("td");
+                //获取父元素的自定义属性,并得到其中从后台传来当前的代表是否注销的数据
+                var tcId = td.attr("data-tcId"); //要得到当前是哪个用户点击注销
+                var status = td.attr("data-status");
+                console.log(tcId +","+ status);
+                //把代表是否注销的数据传送给后台(后台会根据点击操作改变权限注销或启用_)
+                $.ajax({
+                    type:"post",
+                    url : "/api/teacher/handle",
+                    data : {
+                        tc_id : tcId,
+                        tc_status : status
+                    },
+                    dataType:"json",
+                    //响应成功后,把后台修改过的权限数据再返回给前台(点击发送是0,会传回1,发送1,会响应回来0)
+                    success:function(data){
+                        console.log(data);
+                        //如果成功,
+                        if(data.code ==200 ){
+                            //修改当前状态
+                            //1.先修改当前状态td中保存权限数据的自定义属性值
+                            td.attr("data-status",data.result.tc_status)
+                            //2.再根据修改后的权限数据更改按钮的内容
+                            if(data.result.tc_status==0){
+                                console.log(that)
+                                that.text("注销");
+                            }else{
+                                that.text("启用");
+                            }
+                        }
+                        }
+                })
+
+            });
+
         }
     })
 
