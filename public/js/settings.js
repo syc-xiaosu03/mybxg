ce6f480e225cc1d19e147c99371ec4ab81c41678
sansuDeSu <syc_kblk@126.com>
2017-09-26 19:40:39 +0800

    完成个人中心表单提交功能

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/25.
  */
-define(["jquery","template","ckeditor","uploadify","region","datepicker","language"],function($,template,CKEDITOR){
+define(["jquery","template","ckeditor","uploadify","region","datepicker","language","validate","form"],function($,template,CKEDITOR){
     //调用接口获取后台信息
     $.ajax({
         type:"get",
@@ -39,6 +39,38 @@ define(["jquery","template","ckeditor","uploadify","region","datepicker","langua
                     {name:"editing",groups:["find","selection0","spellchecker","editing"]}
                 ]
             });
+
+            //处理表单提交
+            //先验证表单
+            $("#settingsForm").validate({
+                sendForm:false,
+                valid:function(){
+                    //获取家乡的信息(根据省市县的值拼接)
+                    var p = $("#p").find("option:selected").text();
+                    var c = $("#c").find("option:selected").text();
+                    var d = $("#d").find("option:selected").text();
+                    var pcd=p+"|"+c+"|"+d;
+                    console.log(pcd);
+                    //同步富文本
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    //成功后 提交向后台发送数据
+                    $(this).ajaxSubmit({
+                        type:"post",
+                        url:"/api/teacher/modify",
+                        data:{tc_hometown:pcd},
+                        dataType:"json",
+                        success:function(data){
+                            console.log(data)
+                            if(data.code==200){
+                                location.reload();
+                            }
+                        }
+                    })
+                }
+
+            })
             
              
         }
